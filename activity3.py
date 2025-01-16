96e74c147c5952cdf081b7fc6eb3a5a0b683e2e7
sarinah <sarinahhaque@gmail.com>
2025-01-16 19:50:38 +1100

    first publish

---
+++ activity3.py
@@ -0,0 +1,10 @@
+print ("enter marks obtain in four subjects :")
+math=int(input("Math:"))
+science=int(input("Science:"))
+english=int(input("English:"))
+history=int(input("History:"))
+sum= math+science+english+history
+print ("sum of math,science,english,history",sum)
+perc=(sum/400)* 100
+print (end="Percentage marks: ")
+print (perc)
