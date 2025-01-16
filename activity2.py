ea3717ce6a82a1a56915688a42ed81fdf3e969aa
sarinah <sarinahhaque@gmail.com>
2025-01-16 19:42:02 +1100

    first publish

---
+++ activity2.py
@@ -0,0 +1,8 @@
+amount=int(input("please enter amount for withdraw :"))
+note_1=amount//100
+note_2=(amount%100)//50
+note_3=((amount%100)//50)//100
+print ("notes of 100 rupees :",note_1)
+print ("notes of 50 rupees :",note_2)
+print ("notes of 10 rupees :",note_3)
+
